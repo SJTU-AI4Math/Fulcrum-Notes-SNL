fd2dd4cbacf549def6d76f94f93730a9758d8c92
subfish-zhou <subfishzhou@gmail.com>
2026-09-06 21:43:12 +0000

    Adopt compact SNL notation and AST precedence support

--- Lean4/Basic Algebra/BasicAlgebra.lean
+++ Lean4/Basic Algebra/BasicAlgebra.lean
@@ -1,12 +1,12 @@
 import Mathlib.Algebra.Group.Defs
 import SNL4Lean
 
-snl_macro "Semigroup" "const" "text" "#0 has Semigroup structure"
-snl_macro "Semigroup.mul_assoc" "const" "text" "Associativity"
-snl_macro "outParam" "const" "text" "#0"
-snl_macro "HMul" "const" "text" "HMul #0 #1 #2"
-snl_macro "Mul.mul" "const" "formula_inline" "#0 \\cdot #1"
-snl_macro "Lean.proj" "rule" "text" "#0.#1"
+snl_notation Semigroup => %#0 has Semigroup structure%
+snl_notation Semigroup.mul_assoc => %Associativity%
+snl_notation outParam => %#0%
+snl_notation HMul => %HMul #0 #1 #2%
+snl_notation Mul.mul => $#0 \cdot #1$
+snl_macro { name := "Lean.proj", kind := "rule", mode := "text", template := "#0.#1" }
 
 /-! Semigroup defined in Mathlib -/
 #print Semigroup
